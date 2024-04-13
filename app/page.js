3019d98b2bf68adc189cc58c370b141e9a2a9802
Priyanshu-dev-source <priyanshu2004ss@gmail.com>
2024-04-14 00:18:57 +0530

    css properties changed

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hi</div>  
+    <>
+      <div>Hi</div>
+      <button className="hover:bg-red-700 h-[69px] w-36 transition duration-150 ease-in-out rounded-lg transform motion-safe:hover:scale-110">hello</button>  
+    </>
   );
 }
